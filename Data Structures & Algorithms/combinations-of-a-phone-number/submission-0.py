44aa9ad36162c94523ae1014568e67d814ccf93d
ben-khong <benkhong27@gmail.com>
2026-09-02 16:28:06 -0700

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,38 @@
+"""
+34
+
+def ghi
+
+                d                       e                       f
+            g   h   i                g    h   i                g    h   i
+"""
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        res = []
+        combo = []
+
+        phoneMap = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
+
+        def dfs(i):
+            if i >= len(digits):
+                res.append("".join(combo.copy()))
+                return
+            
+            for digit in digits[i]:
+                for letter in phoneMap[digit]:
+                    combo.append(letter)
+                    dfs(i+1)
+                    combo.pop()
+        dfs(0)
+        return res 
