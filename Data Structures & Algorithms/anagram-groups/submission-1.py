c66d6aa8e72cdfa62d8609be206b289dab3c9271
ben-khong <benkhong27@gmail.com>
2026-09-08 07:05:12 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,15 @@
+"""
+Use a hashmap with key as character count and list of anagrams
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        my_map = defaultdict(list)
+        for string in strs:
+            key = [0] * 26
+            for c in string:
+                key[ord(c)-ord('a')] += 1
+            key = tuple(key)
+            my_map[key].append(string)
+        return list(my_map.values())
+            
+
