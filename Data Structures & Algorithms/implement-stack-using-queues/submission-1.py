3868fc4c344ce5a002708173a4568e096b90fb60
ben-khong <benkhong27@gmail.com>
2026-09-14 13:06:31 -0700

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.py
@@ -0,0 +1,44 @@
+"""
+implement a stack using queues
+stack is LIFO, q is FIFO
+
+can only use push to back, peek/pop from front, size, and is empty 
+
+append(), self.stack[0], stack.popleft()
+
+have two queues, one to imitate the stack and the other to keep track of the order
+
+[1, 2]
+
+[1, 2]
+
+use len(arr)-1
+"""
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q)-1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+
+    def top(self) -> int:
+        return self.q[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+        
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
